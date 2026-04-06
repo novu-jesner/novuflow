90cdba2c097bbf5c6b30926a0851c24118a7186a
Znedu <pdenzashley@gmail.com>
2026-04-06 14:30:45 +0800

    add register link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,15 +33,21 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+    @if (Route::has('password.request'))
+        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100"
+           href="{{ route('password.request') }}">
+            {{ __('Forgot your password?') }}
+        </a>
+    @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+    <x-primary-button>
+        {{ __('Log in') }}
+    </x-primary-button>
+
+    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100"
+       href="{{ route('register') }}">
+        {{ __('Register') }}
+    </a>
         </div>
     </form>
 </x-guest-layout>
